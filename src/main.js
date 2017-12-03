dae2d72de1a38c551adefaead54186c037df823c
meganr <meganrm@gmail.com>
2017-12-02 16:52:04 -0800

    set up url request

--- src/main.js
+++ src/main.js
@@ -3,15 +3,34 @@ import ReactDom from 'react-dom';
 
 import './styles/app.scss';
 
+import getData from './logics/getData';
 import Header from './components/header';
-import CowsayButton from './components/cowsayButton';
+import SearchForm from './components/searchForm';
+import SearchResultList from './components/searchResultList';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.getData = this.getData.bind(this);
+    this.state = {
+      redditList: [],
+    };
+  }
+
+  getData(searchFormBoard, searchFormLimit) {
+    const url = `http://www.reddit.com/r/${searchFormBoard}.json?limit=${searchFormLimit}`;
+    getData(url).then((result) => {
+      const redditList = result.body.data.children;
+      this.setState({ redditList });
+    });
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <CowsayButton />
+        <SearchForm getData={this.getData} />
+        <SearchResultList redditList={this.state.redditList} />
       </div>
     );
   }
