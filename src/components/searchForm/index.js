3f77362c770502a613152fdcf3cf9da9cee2c31a
meganr <meganrm@gmail.com>
2017-12-02 16:53:08 -0800

    search form initalizes search, results list renders

--- src/components/searchForm/index.js
+++ src/components/searchForm/index.js
@@ -2,14 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.state = {
+      searchFormBoard: '',
+      searchBoardLimit: 30,
+    };
+  }
+
+  onChange(event) {
+    const key = event.target.id;
+    const value = event.target.value.toLowerCase();
+    this.setState({ [key]: value });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.getData(this.state.searchFormBoard, this.state.searchBoardLimit);
+  }
+
   render() {
     return (
+      <form onSubmit={this.onSubmit}>
+        <label htmlFor="searchFormBoard">Form Board
+          <input type="text" id="searchFormBoard" onChange={this.onChange} />
+        </label>
+        <label htmlFor="searchBoardLimit">Number of results
+          <input type="text" value="30" id="searchBoardLimit" onChange={this.onChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
     );
   }
 }
 
 SearchForm.propTypes = {
-  cowSays: PropTypes.string.isRequired,
+  getData: PropTypes.func.isRequired,
 };
 
 export default SearchForm;
--- src/components/searchResultList/index.js
+++ src/components/searchResultList/index.js
@@ -1,32 +1,22 @@
 import React from 'react';
-import { say } from 'cowsay';
-import getFakerData from '../../logics/getFakerData';
-import CowSays from '../cowsay';
+import PropTypes from 'prop-types';
 
-class CowsayButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.onClick = this.onClick.bind(this);
-    this.state = {
-      cowSay: '',
-    };
-  }
-
-  onClick() {
-    const cowSayText = `Hello ${getFakerData()}`;
-    const cowSay = say({ text: cowSayText });
-    this.setState({ cowSay });
-    console.log();
-  }
-
+class SearchResultList extends React.Component {
   render() {
+    const {
+      redditList,
+    } = this.props;
+
+    const listItems = redditList.map(ele =>
+      <li key={ele.data.id}><a target="_blank" href={ele.data.url}>{ele.data.title}</a></li>);
     return (
-      <div>
-        <button onClick={this.onClick}>Cow speak </button>
-        <CowSays cowSays={this.state.cowSay} />
-      </div>
+      <ul>{listItems}</ul>
     );
   }
 }
 
-export default CowsayButton;
+SearchResultList.propTypes = {
+  redditList: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+export default SearchResultList;
